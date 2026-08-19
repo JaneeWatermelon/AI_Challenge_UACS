509de90d2ce615ed9742a42c52c86736230a524f
Danila <efimovdanila.de@gmail.col>
2026-08-19 19:39:55 +0400

    added the action verdict model

---
+++ agent/actions/verdict.py
@@ -0,0 +1,39 @@
+"""
+@file agent/actions/verdict.py
+"""
+
+from enum import Enum
+
+
+class ExitCode(Enum):
+    SUCCESS = "success"
+    LOGIC_ERROR = "logic_error"
+    PERMISSION_DENIED = "permission_denied"
+    INVALID_ARGUMENT = "invalid_argument"
+    RUNTIME_ERROR = "runtime_error"
+
+
+class ActionVerdict:
+    def __init__(self, code: ExitCode, details: str = ""):
+        self.code = code
+        self.details = details
+
+    @property
+    def success(self) -> bool:
+        return self.code is ExitCode.SUCCESS
+
+    def to_json(self) -> dict:
+        return {
+            "code": self.code.value,
+            "details": self.details,
+        }
+
+    @classmethod
+    def from_json(cls, data: dict) -> "ActionVerdict":
+        return cls(
+            code=ExitCode(data["code"]),
+            details=data.get("details", ""),
+        )
+
+    def __bool__(self) -> bool:
+        return self.success
