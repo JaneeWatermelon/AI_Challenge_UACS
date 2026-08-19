398c890cf63c8e573f405f408ab21dc4fc927ab4
Danila <efimovdanila.de@gmail.col>
2026-08-20 00:33:52 +0400

    fs service api updated

--- agent/utils/paths.py
+++ agent/utils/paths.py
@@ -2,6 +2,7 @@
 @file agent/utils/paths.py
 """
 
+import os
 from pathlib import Path
 from typing import List
 
@@ -9,8 +10,13 @@ from typing import List
 class FsService:
 
     def __init__(self,
-                 #env
+                 #TODO: environment
                  ):
+        #self.environment = environment
+        ...
+
+
+    def resolve_path(self, path: Path) -> Path:
         ...
 
 
@@ -19,25 +25,91 @@ class FsService:
         ...
 
 
+    def _path_assert(self, path: Path) -> None:
+        full_path = self.resolve_path(path)
+        if not self.is_path_allowed(full_path):
+            raise ValueError(f"path is not allowed in the environment: {str(path)}")
+
+
+    def _dir_assert(self, path: Path) -> None:
+        full_path = self.resolve_path(path)
+
+        if not full_path.exists():
+            raise FileNotFoundError(f"directory not found: {str(path)}")
+
+        if not full_path.is_dir():
+            raise NotADirectoryError(f"expected path to a directory, given: {str(path)}")
+
+
+    def _file_assert(self, path: Path) -> None:
+        full_path = self.resolve_path(path)
+
+        if not full_path.exists():
+            raise FileNotFoundError(f"file not found: {str(path)}")
+
+        if not full_path.is_file():
+            raise NotADirectoryError(f"expected path to a file, given: {str(path)}")
+
+
     @staticmethod
     def validate_path(path: Path) -> bool:
-        ...
+        if len(str(path)) > 4096:
+            return False
+
+        try:
+            path_obj = Path(path)
+        except ValueError as e:
+            return False
+
+        if path_obj.is_absolute():
+            return False
+
+        return True
 
 
     def mkdir(self, path: Path, parents: bool=False) -> None:
-        ...
+        self._path_assert(path)
+
+        full_path = self.resolve_path(path)
+
+        full_path.mkdir(parents=parents, exist_ok=True)
 
 
-    def listdir(self, directory: Path) -> List[Path]:
-        ...
+    def listdir(self, path: Path) -> List[Path]:
+        self._path_assert(path)
+        self._dir_assert(path)
+
+        full_path = self.resolve_path(path)
+
+        return list(full_path.iterdir())
 
 
-    def create_file(self, path: Path, content) -> None:
-        ...
+    def create_file(self, path: Path, content: List[str] | None=None) -> None:
+        self._path_assert(path)
+
+        full_path = self.resolve_path(path)
+        if full_path.exists():
+            raise RuntimeError(f"file already exists: {str(path)}")
+
+        full_path.touch(exist_ok=False)
+
+        if content is not None:
+            full_path.write_text("\n".join(content))
 
 
     def remove(self, path: Path) -> None:
-        ...
+        self._path_assert(path)
+
+        full_path = self.resolve_path(path)
+        os.remove(full_path)
+
+
+    def rename(self, path: Path, target: str) -> None:
+        self._path_assert(path)
+
+        full_path = self.resolve_path(path)
+
+        full_path.rename(target)
 
 
     def readline(self, path: Path, idx: int) -> str:
@@ -48,7 +120,7 @@ class FsService:
         ...
 
 
-    def insertline(self, path: Path, idx: int, content: str) -> None:
+    def insertline(self, path: Path, base: int, content: str) -> None:
         ...
 
 
@@ -56,7 +128,7 @@ class FsService:
         ...
 
 
-    def insertlines(self, path: Path, idx: int, content: List[str]) -> None:
+    def insertlines(self, path: Path, base: int, content: List[str]) -> None:
         ...
 
 
