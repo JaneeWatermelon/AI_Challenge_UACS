903606d41d16d62140d6c4f23105e9bcded34942
Danila <efimovdanila.de@gmail.col>
2026-08-19 19:48:18 +0400

    added action interface

---
+++ agent/actions/base.py
@@ -0,0 +1,27 @@
+"""
+@file agent/actions/base.py
+"""
+
+from abc import ABC, abstractmethod
+from typing import Dict, Any
+
+from .verdict import ActionVerdict
+
+
+class Action(ABC):
+
+    def __init__(self,
+                 name: str,
+                 description: str,
+                 arguments: Dict[str, Any]):
+        self.name = name
+        self.desc = description
+        self.args = arguments
+
+    @abstractmethod
+    def to_json(self) -> Dict[str, Any]:
+        pass
+
+    @abstractmethod
+    def execute(self) -> ActionVerdict:
+        pass
--- agent/actions/verdict.py
+++ agent/actions/verdict.py
@@ -14,6 +14,7 @@ class ExitCode(Enum):
 
 
 class ActionVerdict:
+
     def __init__(self, code: ExitCode, details: str = ""):
         self.code = code
         self.details = details
