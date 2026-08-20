2f971027c65a874a24c9e167ddb120d76a234282
Danila <efimovdanila.de@gmail.col>
2026-08-20 15:29:24 +0400

    regex.py interface cutted

--- agent/utils/regex.py
+++ agent/utils/regex.py
@@ -3,44 +3,25 @@
 """
 
 import re
-from re import Pattern, Match
-from typing import Iterable, Iterator
+from typing import Pattern
 
 
 class RegexService:
 
     @staticmethod
     def compile(
-        pattern: str,
-        *,
-        regex: bool = True,
-        case_sensitive: bool = False,
-        whole_word: bool = False,
+            pattern: str,
+            regex: bool = True,
+            case_sensitive: bool = False,
+            whole_word: bool = False
     ) -> Pattern[str]:
+
         if not regex:
             pattern = re.escape(pattern)
 
         if whole_word:
             pattern = rf"\b(?:{pattern})\b"
 
-        flags = 0
-
-        if not case_sensitive:
-            flags |= re.IGNORECASE
+        flags = 0 if case_sensitive else re.IGNORECASE
 
         return re.compile(pattern, flags)
-
-
-    @staticmethod
-    def search(pattern: Pattern[str], text: str) -> Match[str] | None:
-        return pattern.search(text)
-
-
-    @staticmethod
-    def finditer(pattern: Pattern[str], text: str) -> Iterator[Match[str]]:
-        return pattern.finditer(text)
-
-
-    @staticmethod
-    def matches(pattern: Pattern[str], text: str) -> bool:
-        return pattern.search(text) is not None
