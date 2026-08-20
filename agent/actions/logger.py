f5ace8ff4c96119d9463220d833a64bb69f4e268
Jane Sinitsyn <sinitsyn.zena@yandex.ru>
2026-08-20 22:33:47 +0400

    Made logger for actions (not tested)

---
+++ agent/actions/logger.py
@@ -0,0 +1,67 @@
+"""
+Contains defenition of logger to store last executed actions
+"""
+
+from base import Action
+from typing import List, Optional
+
+class ActionLogger:
+    """
+    Singleton logger for storing actions
+
+    Params:
+        _instance (ActionLogger | None): the only one instance of ActionLogger.
+        actions (List[:obj:`Action`]): Stack which stores actions. 
+    """
+
+    _instance = None
+    actions: List[Action] = []
+
+    def __new__(cls, *args, **kwargs):
+        """
+        Singleton pattern
+        """
+        if cls._instance is None:
+            cls._instance = super(ActionLogger, cls).__new__(cls, *args, **kwargs)
+        return cls._instance
+    
+
+    def __init__(self):
+        """
+        Creates an instance of ActionLogger
+        """
+
+    def push(self, action: Action) -> None:
+        """
+        Push an action to logger's stack.
+
+        Args:
+            action (:obj:`Action`): Action which is going to be pushed.
+        """
+        self.actions.append(action)
+
+    def pop(self) -> Optional[Action]:
+        """
+        Pops an action from logger's stack.
+
+        Returns:
+            Action or None: if exists - popped action, else - None.
+        """
+        if len(self.actions) > 0:
+            return self.actions.pop()
+        else:
+            return None
+        
+    def pops(self, n: int) -> List[Action]:
+        """
+        Pops the specified amount of actions from logger's stack.
+
+        Returns:
+            List[Action]: List of actions.
+        """
+        first_index = max(len(self.actions)-n, 0)
+        result = self.actions[first_index:]
+        self.actions = self.actions[:first_index]
+
+        return result
+
