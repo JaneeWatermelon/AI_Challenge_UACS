7f6dfc1f50515a2ea64b8353cae1ae6efeba9c09
Danila <efimovdanila.de@gmail.col>
2026-08-20 13:57:46 +0400

    single reading option removed

--- agent/actions/workspace/read.py
+++ agent/actions/workspace/read.py
@@ -8,6 +8,7 @@ from pathlib import Path
 from ..base import Action
 from ..verdict import ActionVerdict, ExitCode
 from ...utils.paths import FsService
+from ...utils.assertion import safe_verdict
 
 
 class ActionRead(Action):
@@ -17,27 +18,13 @@ class ActionRead(Action):
                  fs_service: FsService):
         super().__init__(
             "read",
-            "reads a single line by index or a range by base and offset, arguments:\n"
+            "reads a range of lines by base and offset, arguments:\n"
             "filename - name of a file to read\n"
-            "line - index of a single line to read\n"
             "base - start index of a range to read from\n"
             "offset - amount of lines to read",
             arguments
         )
         self.fs = fs_service
-
-
-    @property
-    def single(self) -> bool:
-        return self.arguments.__contains__("line")
-
-
-    def _readline(self, path: Path, idx: int) -> str:
-        return self.fs.readline(path, idx)
-
-
-    def _readlines(self, path: Path, base: int, offset: int) -> List[str]:
-        return self.fs.readlines(path, base, offset)
 
 
     @override
@@ -50,6 +37,7 @@ class ActionRead(Action):
 
 
     @override
+    @safe_verdict
     def execute(self) -> ActionVerdict:
         path = self.arguments.get("filename", "")
 
@@ -59,35 +47,11 @@ class ActionRead(Action):
                 "missed 'filename' argument"
             )
 
-        try:
-            full_path = self.fs.resolve_path(Path(path))
+        full_path = self.fs.resolve_path(Path(path))
 
-            if self.single:
-                idx = self.arguments.get("line")
-                content = self._readline(full_path, idx)
-
-            else:
-                base = self.arguments.get("base")
-                offset = self.arguments.get("offset")
-                content = self._readlines(full_path, base, offset)
-
-        except PermissionError as e:
-            return ActionVerdict(
-                ExitCode.PERMISSION_DENIED,
-                str(e)
-            )
-
-        except ValueError as e:
-            return ActionVerdict(
-                ExitCode.INVALID_ARGUMENT,
-                str(e)
-            )
-
-        except Exception as e:
-            return ActionVerdict(
-                ExitCode.EXECUTION_ERROR,
-                str(e)
-            )
+        base = self.arguments.get("base")
+        offset = self.arguments.get("offset")
+        content = self.fs.readlines(full_path, base, offset)
 
         return ActionVerdict(
             ExitCode.SUCCESS,
