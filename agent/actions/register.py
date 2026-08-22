c5a521680bac90124ae79f160432e6027371a693
DanilaEfimov <danilaefimov.ed@gmail.com>
2026-08-22 22:39:33 +0300

    register.py updated

--- agent/actions/register.py
+++ agent/actions/register.py
@@ -2,7 +2,7 @@
 @file agent/actions/register.py
 """
 
-from typing import Dict
+from typing import Dict, Optional
 
 from AI_Challenge_UACS.agent.actions.base import Action
 
@@ -14,6 +14,12 @@ class ActionRegister:
 
     _register: Dict[str, type[Action]] = {}
 
+
     @staticmethod
     def add_action(name: str, action_type: type[Action]) -> None:
-        ActionRegister.register[name] = action_type
+        ActionRegister._register[name] = action_type
+
+
+    @staticmethod
+    def get_action(name: str) -> Optional[type[Action]]:
+        return ActionRegister._register.get(name)
