e5eb33b597ff5ee01acab60256e0009aaec6c91a
DanilaEfimov <danilaefimov.ed@gmail.com>
2026-08-23 00:07:20 +0300

    dispatcher interface finished

--- agent/actions/dispatcher.py
+++ agent/actions/dispatcher.py
@@ -2,48 +2,49 @@
 @file agent/actions/dispatcher.py
 """
 
-import json
-from json import JSONDecodeError
 from typing import List, Optional, Dict, Any
 
-from .base import Action
+from base import Action
 from verdict import ActionVerdict, ExitCode
+from AI_Challenge_UACS.agent.protocol.parser import BotResponseParser
+from AI_Challenge_UACS.agent.protocol.format import BotRequiredFields
 
 
 class ActionDispatcher:
 
-    def __init__(self):
-        pass
 
+    def dispatch(self, raw_input: str) -> Optional[List[ActionVerdict]]:
+        parse_verdict, actions = self._parse(raw_input)
 
-    def dispatch(self, raw_input: str) -> Optional[ActionVerdict]:
-        parsed = self._parse(raw_input)
-        action = self._select_action(parsed)
-
-        if action is None:
-            return ActionVerdict(
-                ExitCode.NO_ACTION_SELECTED,
-                "failed to select an action"
-            )
+        if parse_verdict.code != ExitCode.SUCCESS:
+            return [parse_verdict]
 
         try:
-            return action.execute()
+            for action in actions:
+                action.execute()
 
         except Exception as e:
+            return [
+                ActionVerdict(
+                    ExitCode.EXECUTION_ERROR,
+                  f"execution aborted at {1}th\n"
+                  f"Execution interrupted.\n"
+                  f"All operations have been cancelled.\n"
+                  f"No changes were applied."
+                )
+            ]
+
+
+    def _parse(self, raw_input: str) -> tuple[ActionVerdict, List[Action]]:
+        json_response = BotResponseParser.to_json(raw_input)
+        parsed = BotResponseParser.parse_required_fields(json_response)
+
+        actions_field = parsed.get(BotRequiredFields.value)
+        if actions_field is None:
             return ActionVerdict(
-                ExitCode.EXECUTION_ERROR,
-                f"Action execution failed: {str(e)}"
-            )
+                ExitCode.PROTOCOL_ERROR,
+                f"required field {BotRequiredFields.ACTIONS.value} missed"
+            ), []
 
+        return BotResponseParser.parse_actions(actions_field)
 
-    def _parse(self, raw_input: str) -> tuple[List[Dict[str, Any]], str]:
-        try:
-            json_reply = json.loads(raw_input)
-
-        except JSONDecodeError as e:
-            return [], str(e)
-
-
-    def _select_action(self, parsed: Dict[str, Any]) -> Optional[Action]:
-        # TODO: actions selection workflow
-        return None
